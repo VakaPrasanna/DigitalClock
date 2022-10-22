03914409bbb6bc110d265781fb985a8075e3db0f
VakaPrasanna <96370281+VakaPrasanna@users.noreply.github.com>
2022-10-22 08:52:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import DigitalClock from "./DigitalClock.js";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <DigitalClock />
+    </div>
+  );
+}
+export default App;
